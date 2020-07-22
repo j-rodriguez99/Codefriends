2fb4d7560e7f8aa58bd8f0a036c7f25e5028f63d
Joshua Segui <joshua.segui.rodriguez@gmail.com>
2020-07-22 15:46:50 -0400

    Game is fully functioning, computer never loses but sometimes chooses defense over victory

--- tic.js
+++ tic.js
@@ -1,45 +1,16 @@
 (async () => {
 
-/* 
-The first priority is to check if a win condition exists. If so, fill that spot. 
-The second priority is to play defense by preventing a win condition for the enemy.
-The thrid priority is to occupy strategic locations. 
-
-In order to accomplish the first priority, you need to check all 8 possible paths to victory. You need to see that you are occupying
-two of the spaces and that the third is unoccupied. 
-
-Defense is similar in that you check if two of the spaces in the victory paths are occupied by the enemy. 
-
-If all three spaces in a victory path are occupied by a player. End the game. 
-
-Ask the player if they would like to play again. 
-*/
-
-// Instructions: 
-
 const { Select } = require('enquirer');
-
-console.log(`\nBelow you will find a tic tac toe board. You can choose your move based on the column headers. For example, a move at 1B will be at the center top row. Moves at 1A, 2B, and 3C would give you a diagonal 3 in a row!\n`);
-
-const header = ['A', 'B', 'C',];
-const ex1 = ['1A', ' ', ' ',]; 
-const ex2 = [' ', '2B', ' ',];
-const ex3 = [' ', ' ', '3C',];
-
-
-console.log(header); console.log('');
-console.log(ex1); console.log('');
-console.log(ex2); console.log('');
-console.log(ex3); console.log('');
+const { Toggle } = require('enquirer');
 
 // variables and arrays needed in game. 
 let gameOn; 
 const board = ['1A', '2A', '3A', '1B', '2B', '3B', '1C', '2C', '3C', 'I don\'t want to play anymore']
 let play; 
 let gameBoard;
-let player = 'Player X'; 
-let placeholder;
 let computerMove;  
+let again;
+let boolean;
 
 // function to remove a particular string from an array. 
 const removeString = (array, string) => {
@@ -51,35 +22,71 @@ const removeString = (array, string) => {
 }
 
 // function that checks a particular victory path. It also informs the computer which position to move to in order to secure a victory or play defense. 
-// the function takes six paramaters representing the 3 array indexes that are a potential 3 in a row and their corresponding string location names. 
+// the function takes six paramaters - 3 array indexes that can make 3 in a row and their corresponding string names. 
+
+// Is it smart enough to choose victory over defense though? 
 
 const winCondition = (a, b, c, A, B, C) => { 
 
-    if (a === b && b === c && c === a && a){
+    if (a === b && b === c && a){
         gameOn = 'we have a winner';
-        console.log(`Congratulations ${player}, you won!`);
+        if (a === 'X'){
+          console.log(`Congratulations Player X, you won!`);}
+        else {console.log(`Congratulations Player O, you won!`);}
     }
-    else if (a === b && !c){computerMove = C;}
-    else if (a === c && !b){computerMove = B;}
-    else if (b === c && !a){computerMove = A;}
+    else if (a === b && !c && a){computerMove = C;}
+    else if (a === c && !b && a){computerMove = B;}
+    else if (b === c && !a && b){computerMove = A;}
 
 }; 
 
 
-// larger loop starts here. 
+// Outer loop starts here. 
 
-gameBoard = [...board]; 
+do {
+  // game instructions. 
+  console.log(`\nBelow you will find a tic tac toe board. You can choose your move based on the column headers. For example, a move at 1B will be at the center top row. Moves at 1A, 2B, and 3C would give you a diagonal 3 in a row!\n`);
 
-const row1 = ['', '', '']; 
-const row2 = ['', '', '']; 
-const row3 = ['', '', '']; 
+  const header = ['A', 'B', 'C',];
+  const ex1 = ['1A', ' ', ' ',]; 
+  const ex2 = [' ', '2B', ' ',];
+  const ex3 = [' ', ' ', '3C',];
+  
+  
+  console.log(header); console.log('');
+  console.log(ex1); console.log('');
+  console.log(ex2); console.log('');
+  console.log(ex3); console.log('');
+
+  again = new Toggle({
+    message: 'Would you like to play again?',
+    enabled: 'Yes',
+    disabled: 'No'
+    });
+
+  // resets the choices that player x can make. 
+  gameBoard = [...board]; 
+
+  gameOn = '';
+
+  // resets the arrays that hold the Xs and Os. 
+
+  const row1 = ['', '', '']; 
+  const row2 = ['', '', '']; 
+  const row3 = ['', '', '']; 
 
 
 // Innerloop starts here. The actual game.
 
 while (!gameOn){
+
+  computerMove = '';
+
+  console.log('It is your turn to play. Choose wisely!');
  
-    play = new Select({
+  // re-create the play variable with updated gameBoard array on every loop. 
+
+  play = new Select({
   name: 'play',
   message: 'Place an X on the board!',
   choices: gameBoard
@@ -101,6 +108,15 @@ while (!gameOn){
     })
   .catch(console.error);
 
+  console.log(header); console.log('');
+  console.log(row1); console.log('');
+  console.log(row2); console.log('');
+  console.log(row3); console.log('');
+
+  if (gameBoard.length === 1){ console.log('\n This game has ended in a stalemate!');
+    break; 
+}
+
 winCondition(row1[0], row2[0], row3[0], '1A', '2A', '3A'); 
 winCondition(row1[1], row2[1], row3[1], '1B', '2B', '3B');
 winCondition(row1[2], row2[2], row3[2], '1C', '2C', '3C');
@@ -110,8 +126,12 @@ winCondition(row3[0], row3[1], row3[2], '3A', '3B', '3C');
 winCondition(row1[0], row2[1], row3[2], '1A', '2B', '3C');
 winCondition(row1[2], row2[1], row3[0], '1C', '2B', '3A');
 
+console.log('The computer will now take it\'s turn.');
+
 if (!computerMove) {
-  if (!row2[1]){computerMove = '2B'}
+  if (row2[1] === 'O' && row1[0] === 'X' && row3[2] === 'X' && !row1[1]){computerMove = '1B';}
+  else if (row2[1] === 'O' && row1[2] === 'X' && row3[0] === 'X' && !row1[1]){computerMove = '1B';}
+  else if (!row2[1]){computerMove = '2B'}
   else if (!row1[2]) {computerMove = '1C'}
   else if (!row3[2]) {computerMove = '3C'}
   else if (!row3[0]) {computerMove = '3A'}
@@ -132,15 +152,37 @@ if (!computerMove) {
       else if (computerMove === '1C') {row1[2] = 'O'; removeString(gameBoard, '1C');}
       else if (computerMove === '2C') {row2[2] = 'O'; removeString(gameBoard, '2C');}
       else if (computerMove === '3C') {row3[2] = 'O'; removeString(gameBoard, '3C');}
+      else {row2[2] = 'ZZZ';}
+
+    console.log(header); console.log('');
+    console.log(row1); console.log('');
+    console.log(row2); console.log('');
+    console.log(row3); console.log('');
 
 
-console.log(header); console.log('');
-console.log(row1); console.log('');
-console.log(row2); console.log('');
-console.log(row3); console.log('');
+    winCondition(row1[0], row2[0], row3[0], '1A', '2A', '3A'); 
+    winCondition(row1[1], row2[1], row3[1], '1B', '2B', '3B');
+    winCondition(row1[2], row2[2], row3[2], '1C', '2C', '3C');
+    winCondition(row1[0], row1[1], row1[2], '1A', '1B', '1C');
+    winCondition(row2[0], row2[1], row2[2], '2A', '2B', '2C');
+    winCondition(row3[0], row3[1], row3[2], '3A', '3B', '3C');
+    winCondition(row1[0], row2[1], row3[2], '1A', '2B', '3C');
+    winCondition(row1[2], row2[1], row3[0], '1C', '2B', '3A');
 
-
-
+// end of while loop. 
 }
 
+console.log('');
+
+await again.run()
+.then(answer => {boolean = answer;
+  if (answer === false){console.log('\nThank you for playing. Goodbye!');}
+  })
+.catch(console.error);
+
+} while (boolean); 
+
 })()
+
+
+
